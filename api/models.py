57e9023e5f2e5b2bc8e4052cd7b31bffab47c4ed
Tsoy <hootor@gmail.com>
2021-04-15 08:38:20 +1000

    upgrade goods

--- api/models.py
+++ api/models.py
@@ -1,4 +1,3 @@
-import os
 
 from django.db import models
 from drf_extra_fields.fields import Base64ImageField
@@ -37,3 +36,19 @@ class Good(models.Model):
     uuid = models.CharField(primary_key=True, max_length=36)
     name = models.CharField(max_length=300)
     uuid_parent = models.CharField(max_length=36)
+    isGroup = models.BooleanField(default=False)
+    article_number = models.CharField(max_length=50, default="")
+
+
+class GoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Good
+        fields = ('uuid', 'name', 'uuid_parent', 'isGroup', 'article_number')
+
+        def create(self, validated_data):
+            uuid = validated_data.pop('uuid')
+            name = validated_data.pop('name')
+            uuid_parent = validated_data.pop('uuid_parent')
+            isGroup = validated_data.pop('isGroup')
+            article_number = validated_data.pop('article_number')
+            return Good.objects.create(uuid=uuid, name=name, uuid_parent=uuid_parent, isGroup=isGroup, article_number=article_number)
